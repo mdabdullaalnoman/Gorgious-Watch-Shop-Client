458ed84373a81dde5ced05c4ff103b06fc15c1ac
mdabdullaalnoman <noman.apparel.4003@gmail.com>
2021-11-14 15:38:55 +0600

    firebase project change

--- src/Firebase/FirebaseConfig.js
+++ src/Firebase/FirebaseConfig.js
@@ -1,11 +1,11 @@
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC9J9y4p4d3pppwejbJvCCMbgDZNx78Fyo",
-  authDomain: "gorgeous-watch-shop.firebaseapp.com",
-  projectId: "gorgeous-watch-shop",
-  storageBucket: "gorgeous-watch-shop.appspot.com",
-  messagingSenderId: "814031161729",
-  appId: "1:814031161729:web:7ff84e1f70a33e047ac080"
+  apiKey: "AIzaSyBadruNaitaXQxF1ixqNpjpIoVkSvg-7bk",
+  authDomain: "gorgeous-watch-shop-4b4e1.firebaseapp.com",
+  projectId: "gorgeous-watch-shop-4b4e1",
+  storageBucket: "gorgeous-watch-shop-4b4e1.appspot.com",
+  messagingSenderId: "442542539601",
+  appId: "1:442542539601:web:d6f3e12fb762eb711d4970"
 };
 
 export default firebaseConfig;
